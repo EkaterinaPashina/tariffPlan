a8cb1e99d00a894e7635b9070e2fd1db297f9c66
EkaterinaPashina <k377at@mail.ru>
2023-10-01 17:00:52 +0500

    added p

--- tariff-plan/src/components/tarif.jsx
+++ tariff-plan/src/components/tarif.jsx
@@ -3,13 +3,14 @@ import './tarif.css';
 
 export default class Tarif extends React.Component {
     render() {
-        const { name, price, speed, theme, } = this.props;
+        const { name, price, speed, } = this.props;
+        const theme = this.props.theme;
         return (
             <div className="card" >
                 <div className={theme.name}><p>Безлимитный {name}</p></div>
-                <div className={theme.price}><span className="card__price-rub">руб </span>{price}<span className="card__price-month">/мес</span></div>
-                <div className={theme.speed}>До {speed} Мбит/сек</div>
-                <div className={theme.value}>Объем включенного<br /> трафика не ограничен</div>
+                <div className={theme.price}><p><span className="card__price-rub">руб </span>{price}<span className="card__price-month">/мес</span></p></div>
+                <div className={theme.speed}><p>До {speed} Мбит/сек</p></div>
+                <div className={theme.value}><p>Объем включенного<br /> трафика не ограничен</p></div>
             </div >
         )
     }
