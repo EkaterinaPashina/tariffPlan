ec02df399a024b6854c7cacc8bf222df85e3db0d
EkaterinaPashina <k377at@mail.ru>
2023-10-08 23:59:30 +0500

    added handleActive

--- tariff-plan/src/components/tarif.jsx
+++ tariff-plan/src/components/tarif.jsx
@@ -4,8 +4,12 @@ import './tarif.css';
 export default class Tarif extends React.Component {
     render() {
         const { name, price, speed, theme } = this.props;
+        const handleActive = (e) => {
+            e.currentTarget.classList.add("active");
+        }
+
         return (
-            <div className={theme.container} >
+            <div className={theme.container} onClick={handleActive} >
                 <div className={theme.name}><p>Безлимитный {name}</p></div>
                 <div className={theme.price}><p><span className={theme.priceRub}>руб </span>{price}<span className={theme.priceMonth}>/мес</span></p></div>
                 <div className={theme.speed}><p>до {speed} Мбит/сек</p></div>
