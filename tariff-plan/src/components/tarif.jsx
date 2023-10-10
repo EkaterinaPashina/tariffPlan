202e3be48e31e0d2a92467d01727373f9b08efc2
EkaterinaPashina <k377at@mail.ru>
2023-10-10 15:12:12 +0500

    added state for constructor and onClick function

--- tariff-plan/src/components/tarif.jsx
+++ tariff-plan/src/components/tarif.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import './tarif.css';
+import classNames from 'classnames';
 
 export default class Tarif extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { choised: false };
+    }
+
+    onChangeClass = () => {
+        this.setState(({ choised }) => ({ choised: !choised }));
+    };
+
     render() {
         const { name, price, speed, theme } = this.props;
-        const handleActive = (e) => {
-            e.currentTarget.classList.add("active");
-        }
+        const choisedClass = classNames([
+            `${theme.container}`,
+            this.state.choised ? 'active' : ' '
+        ]);
 
         return (
-            <div className={theme.container} onClick={handleActive} >
+            <div className={choisedClass} onClick={this.onChangeClass}>
                 <div className={theme.name}><p>Безлимитный {name}</p></div>
                 <div className={theme.price}><p><span className={theme.priceRub}>руб </span>{price}<span className={theme.priceMonth}>/мес</span></p></div>
                 <div className={theme.speed}><p>до {speed} Мбит/сек</p></div>
